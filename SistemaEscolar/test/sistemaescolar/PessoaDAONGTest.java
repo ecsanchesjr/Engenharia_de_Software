69628641fdd3a06ae54a822762070cd8da59f5df
ecsanchesjr <ecsanchesjr@hotmail.com>
2017-04-07 00:15:54 -0300

    DAONGtest

---
+++ SistemaEscolar/test/sistemaescolar/PessoaDAONGTest.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemaescolar;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import static org.testng.Assert.*;
+import org.testng.annotations.Test;
+import static sistemaescolar.PessoaDAO.insertPessoaV;
+
+/**
+ *
+ * @author ecsan
+ */
+public class PessoaDAONGTest {
+    
+    public PessoaDAONGTest() {
+    }
+    
+    @Test
+    public void insertPessoaVTest() throws SQLException{
+        insertPessoaV("Elcio Cezario Sanches Junior",  "666.666.666-66", "77.777.777-7", 21);
+        Connection con = ConexaoBD.getCon();
+        Statement s = con.createStatement();
+        ResultSet rs = s.executeQuery("SELECT * FROM Pessoa WHERE pessoa_cpf = '666.666.666-66'");
+        boolean control = (rs!=null);
+        assertTrue(control);
+    }   
+}
