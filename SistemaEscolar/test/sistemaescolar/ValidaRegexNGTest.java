be25f11b319d56b31c53d96228be3a77dba51346
anamandello <ana.mandello@gmail.com>
2017-04-06 23:01:28 -0300

    Novo teste

---
+++ SistemaEscolar/test/sistemaescolar/ValidaRegexNGTest.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemaescolar;
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+import org.testng.annotations.Test;
+/**
+ *
+ * @author Ana Flavia
+ */
+public class ValidaRegexNGTest {
+    
+    ValidaRegex validaregex = new ValidaRegex();
+    
+    public ValidaRegexNGTest() {
+    }
+    
+    @Test
+    public void validaNumerosTest(){
+        StringBuilder msg = new StringBuilder("");
+        assertTrue(ValidaRegex.validaNumeros("52", msg));
+    }
+    
+    @Test
+    public void validaNumerosTest2(){
+        StringBuilder msg = new StringBuilder("");
+        assertFalse(ValidaRegex.validaNumeros("f", msg));
+    }
+    
+
+}
