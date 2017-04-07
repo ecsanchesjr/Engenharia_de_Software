bcba2d1b53ffe111b2c3a17a34e0a94d464d5ff5
luisstelmastchuk <luis.stelmastchuk@hotmail.com>
2017-04-06 21:41:30 -0300

    Imports

--- SistemaEscolar/test/sistemaescolar/ValidaNGTest.java
+++ SistemaEscolar/test/sistemaescolar/ValidaNGTest.java
@@ -5,9 +5,12 @@
  */
 package sistemaescolar;
 
-import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertTrue;
-import org.testng.annotations.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+
+
 
 
 /**
